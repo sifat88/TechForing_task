511af298165da79b952779acdc5e9b2ff98a4d0e
sifat88 <rakibulislam8sifat@gmail.com>
2021-06-20 16:01:45 +0600

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+const siteAddress = document.getElementById("siteAddress");
+const siteMail = document.getElementById("siteEmail");
+
+const inputs = [siteAddress , siteMail];
+inputs.forEach( input => {
+    input.addEventListener("change" , (e)=> {
+        console.log(e.target.value);
+        if(e.target.value.trim().length > 0){
+            input.classList.remove("input_error")
+        }
+    })
+})
+
+
+const formModal = new bootstrap.Modal(document.getElementById('formModal'));
+document.getElementById("freescan").addEventListener("click" , function(e) {
+    e.preventDefault();
+    if(siteAddress.value.trim().length === 0 && siteAddress.value.trim().length === 0){
+        inputs.forEach( input => input.classList.add("input_error"))
+    } else {
+        formModal.show()
+    }
+    console.log("click")
+});
+
+const arrows = document.querySelectorAll(".footer__links h4 img");
+
+arrows.forEach( arrw => {
+    arrw.addEventListener("click", ()=> {
+        arrw.closest('.footer__links').classList.toggle("links_open");
+    })
+})
